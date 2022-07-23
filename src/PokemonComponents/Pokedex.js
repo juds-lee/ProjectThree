fcb88b3a35dd1cb0c55f94763444f2376dbdcd1a
juds-lee <judy_96_@hotmail.com>
2022-07-23 14:54:43 -0400

    pulling data from firebase anf store objects into workable variables

--- src/PokemonComponents/Pokedex.js
+++ src/PokemonComponents/Pokedex.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { useState } from 'react';
 import Popup from 'reactjs-popup';
+import app from '../firebase';
+import { ref, getDatabase, onValue } from 'firebase/database';
+import { UserAuth } from '../contexts/AuthContexts';
 
 const PokeDex = (props) => {
-  function handlePokemonSprites() {
+  const {user} = UserAuth();
+  const [pokemonInfo, setPokemonInfo] = useState([]);
+
+  const callPokeFromFirebase = () => {
+    // this will grab the pokemon stored into the users firebase in order to display it when the modal is clicked
+    const database = getDatabase(app);
+    const dbRef = ref(database, `users/${user?.uid}`);
+    onValue(dbRef, (response) => {
+      // create an empty array to hold our pokemon objects in
+      const pokemonArray = [];
+      const data = response.val();
+      for (let key in data) {
+        pokemonArray.push({key: key, 
+          pokemon: data[key]});
+      }
+      setPokemonInfo(pokemonArray);
+    })
+    console.log(pokemonInfo)
   }
+  
+
   return(
       <div>
         <section>
@@ -15,7 +38,9 @@ const PokeDex = (props) => {
               src="/images/pokedex.jpeg" 
               alt="image of a pokedex" 
               className='pokedexImg'
+              onClick={callPokeFromFirebase}
               />
+
               </button>}
               modal
               nested>
@@ -33,7 +58,6 @@ const PokeDex = (props) => {
                     return (
                       <div className='pokemonContainer' key={pokemon.id}>
                         <div 
-                        onClick={(e) => {handlePokemonSprites()}}
                         className='pokemonSpritesBox' 
                         >
                         <img
